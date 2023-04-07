9030463ca09710b786f42c5b3c36e50592ac0c13
aaliyan1230 <aaliyan1230@gmail.com>
2023-04-07 17:07:36 +0500

    declared store

--- src/state/store/store.js
+++ src/state/store/store.js
@@ -1,6 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import cartActiveReducer from "../cartActive/cartActiveSlice";
 import productsReducer from "../products/productsSlice";
 import { cartReducer } from "../reducers/cart";
 import { api } from "../api/apiSlice";
 
+export const store = configureStore({
+  reducer:{
+    [api.reducerPath]: api.reducer,
+    cartActive: cartActiveReducer,
+    products: productsReducer,
+    cart: cartReducer,
+  },
+  middleware: (getDefaultMiddleware)=>
+    getDefaultMiddleware().concat(api.middleware),
+})
